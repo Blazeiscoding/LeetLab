845e5ff705338f031f651aa636f10d9469952fca
Nikhil Rathore <nikhilrathore52297@gmail.com>
2025-05-03 09:08:19 +0530

    Sure

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/problems", problemRoutes);
-
+app.use("/api/v1/execute-code",executionRoutes)
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port ", process.env.PORT);
 });
