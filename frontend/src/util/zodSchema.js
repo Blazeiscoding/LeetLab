fea4e1a1b37e572a40dd4edc3dd64f17d02b1471
Nikhil Rathore <nikhilrathore52297@gmail.com>
2025-05-14 01:45:52 +0530

    feat: implement routing and authentication structure with Home, Login, and Signup pages; add Zod validation schema for signup

---
+++ frontend/src/util/zodSchema.js
@@ -0,0 +1,7 @@
+import { z } from "zod";
+
+export const signUpSchema = z.object({
+  email: z.string().email("Enter a valid email"),
+  password: z.string().min(6, "Password must be at least 6 characters"),
+  name: z.string().min(3, "Name must be at least 3 characters"),
+});
