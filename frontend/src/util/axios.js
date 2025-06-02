d33c58c2c55a06c9b4d384cf314ebe8e3505271a
Nikhil Rathore <nikhilrathore52297@gmail.com>
2025-06-02 22:22:13 +0530

    feat: Simplify axios instance baseURL configuration and add Vercel rewrites configuration

--- frontend/src/util/axios.js
+++ frontend/src/util/axios.js
@@ -1,10 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.MODE === "development"
-      ? import.meta.env.VITE_BACKEND_URL || "http://localhost:8080/api/v1"
-      : "/api/v1",
-
+  baseURL: import.meta.env.VITE_BACKEND_URL || "http://localhost:8080/api/v1",
   withCredentials: true,
 });
+
+export default axiosInstance;
